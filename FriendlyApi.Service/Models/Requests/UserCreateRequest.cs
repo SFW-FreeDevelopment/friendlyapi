a3a01113ee48148ab107181963c193fb24402886
Christopher Plummer <cplummer@reservations.com>
2021-09-09 20:48:50 -0400

    Changed UserCreateRequest to UserCreateUpdateRequest

--- FriendlyApi.Service/Models/Requests/UserCreateRequest.cs
+++ FriendlyApi.Service/Models/Requests/UserCreateRequest.cs
@@ -2,13 +2,16 @@
 
 namespace FriendlyApi.Service.Models.Requests
 {
-    public class UserCreateRequest
+    public class UserCreateUpdateRequest
     {
         [Required]
         public string Username { get; set; }
+        
         [Required]
         public string Password { get; set; }
+        
         public string Email { get; set; }
+        
         public string PhoneNumber { get; set; }
     }
 }
