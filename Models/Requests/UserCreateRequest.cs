8661eefacd95953cc410e4f23e05009ed74a2d93
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-10 21:45:31 -0400

    Added validation attributes

--- Models/Requests/UserCreateRequest.cs
+++ Models/Requests/UserCreateRequest.cs
@@ -5,13 +5,19 @@ namespace FriendlyApi.Service.Models.Requests
     public class UserCreateRequest
     {
         [Required]
+        [MinLength(2)]
+        [MaxLength(30)]
         public string Username { get; set; }
         
         [Required]
+        [MinLength(6)]
+        [MaxLength(20)]
         public string Password { get; set; }
         
+        [EmailAddress]
         public string Email { get; set; }
         
+        [Phone]
         public string PhoneNumber { get; set; }
     }
 }
--- Models/Requests/UserUpdateRequest.cs
+++ Models/Requests/UserUpdateRequest.cs
@@ -1,13 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace FriendlyApi.Service.Models.Requests
 {
     public class UserUpdateRequest
     {
+        [MinLength(2)]
+        [MaxLength(30)]
         public string Username { get; set; }
         
+        [MinLength(6)]
+        [MaxLength(20)]
         public string Password { get; set; }
         
+        [EmailAddress]
         public string Email { get; set; }
         
+        [Phone]
         public string PhoneNumber { get; set; }
     }
 }
