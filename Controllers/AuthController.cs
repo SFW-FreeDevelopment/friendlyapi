86d9156ed2c4c1b42a077655a8b0ca5cfaac7c04
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-19 19:33:04 -0400

    Hooked up new controller

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using FriendlyApi.Service.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FriendlyApi.Service.Controllers
+{
+    [ApiController]
+    [Route("auth")]
+    public class AuthController : ControllerBase
+    {
+        private readonly AuthService _service;
+        
+        public AuthController(AuthService service)
+        {
+            _service = service;
+        }
+        
+        [HttpPost("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public async Task<bool> Authenticate([FromRoute] string id, [FromBody] string password)
+        {
+            var isValid = await _service.Authenticate(id, password);
+            return isValid;
+        }
+    }
+}
