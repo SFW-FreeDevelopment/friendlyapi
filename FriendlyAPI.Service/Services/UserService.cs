1971f1f0021b638fd8bdf5694b433350760a66b9
Steven Garberg <steven.garberg.jr@gmail.com>
2021-09-20 23:38:59 -0400

    Removed try/catch pattern and added new ctor mapping

--- FriendlyAPI.Service/Services/UserService.cs
+++ FriendlyAPI.Service/Services/UserService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System.Web.Http;
+using FriendlyApi.Service.Exceptions;
 using FriendlyApi.Service.Interfaces;
 using FriendlyApi.Service.Models;
 using FriendlyApi.Service.Models.Requests;
@@ -19,98 +20,59 @@ namespace FriendlyApi.Service.Services
 
         public async Task<IEnumerable<User>> GetAll()
         {
-            try
-            {
-                return await _repository.GetAll();
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            return await _repository.GetAll();
         }
         
         public async Task<User> GetById(Guid id)
         {
-            try
-            {
-                return await _repository.GetById(id.ToString());
-            }
-            catch (HttpResponseException e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            var user = await _repository.GetById(id.ToString());
+            
+            if (user == null)
+                throw new NotFoundException(id);
+            
+            return user;
         }
         
         public async Task<User> Create(UserCreateRequest request)
         {
-            try
-            {
-                User newUser = new User
-                {
-                    Id = Guid.NewGuid().ToString(),
-                    Username = request.Username,
-                    Password = BCrypt.Net.BCrypt.HashPassword(request.Password),
-                    Email = request.Email,
-                    PhoneNumber = request.PhoneNumber
-                };
-                
-                return await _repository.Create(newUser);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            var user = new User(request);
+
+            return await _repository.Create(user);
         }
         
         public async Task<User> Update(Guid id, UserUpdateRequest request)
         {
-            try
+            var user = await _repository.GetById(id.ToString());
+            
+            if (user == null)
+                throw new NotFoundException(id);
+
+            if (!string.IsNullOrEmpty(request.Username))
             {
-                User user = await _repository.GetById(id.ToString());
+                user.Username = request.Username;
+            }
 
-                if (!string.IsNullOrEmpty(request.Username))
-                {
-                    user.Username = request.Username;
-                }
+            if (!string.IsNullOrEmpty(request.Password))
+            {
+                user.Password = BCrypt.Net.BCrypt.HashPassword(request.Password);
+            }
 
-                if (!string.IsNullOrEmpty(request.Password))
-                {
-                    user.Password = BCrypt.Net.BCrypt.HashPassword(request.Password);
-                }
+            if (!string.IsNullOrEmpty(request.Email))
+            {
+                user.Email = request.Email;
+            }
 
-                if (!string.IsNullOrEmpty(request.Email))
-                {
-                    user.Email = request.Email;
-                }
-
-                if (!string.IsNullOrEmpty(request.PhoneNumber))
-                {
-                    user.PhoneNumber = request.PhoneNumber;
-                }
+            if (!string.IsNullOrEmpty(request.PhoneNumber))
+            {
+                user.PhoneNumber = request.PhoneNumber;
+            }
                 
-                return await _repository.Update(id.ToString(), user);
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            return await _repository.Update(id.ToString(), user);
         }
         
         public async Task Delete(Guid id)
         {
-            try
-            {
-                await _repository.Delete(id.ToString());
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
-            }
+            await _repository.Delete(id.ToString());
         }
     }
 }
