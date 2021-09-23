bf273082a983477b78a38063e277dc048d6eafbf
Christopher Plummer <cplummer@reservations.com>
2021-09-22 23:49:58 -0400

    Add UserProfile for GetById on User

--- FriendlyAPI.Service/Services/UserService.cs
+++ FriendlyAPI.Service/Services/UserService.cs
@@ -27,6 +27,8 @@ namespace FriendlyApi.Service.Services
         public async Task<User> GetById(Guid id)
         {
             var user = await _repository.GetById(id.ToString());
+            // TODO: Add validation in case we get an error calling the profile
+            user.Profile = await _profileRepository.GetById(id.ToString());
             
             if (user == null)
                 throw new NotFoundException(id);
