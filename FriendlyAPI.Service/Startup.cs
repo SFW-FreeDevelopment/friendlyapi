b2e6f408dd66405bc9b9b9856f5d20cbcb825782
Christopher Plummer <cplummer@reservations.com>
2021-09-23 20:47:17 -0400

    Added CORS to allow any request

--- FriendlyAPI.Service/Startup.cs
+++ FriendlyAPI.Service/Startup.cs
@@ -25,6 +25,13 @@ namespace FriendlyApi.Service
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
+            {
+                builder.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader();
+            }));
+            
             services.AddScoped<IMongoRepository<User>, UserRepository>();
             services.AddScoped<IMongoRepository<UserProfile>, UserProfileRepository>();
 
